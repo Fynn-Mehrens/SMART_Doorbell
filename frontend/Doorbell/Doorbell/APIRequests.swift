f09f87eae35c9b90abdb8f7a95a088860a2403f0
Cora Bleier <58000612+CoraJB@users.noreply.github.com>
2023-01-26 13:56:35 +0100

    Remaining frontend changes

---
+++ frontend/Doorbell/Doorbell/APIRequests.swift
@@ -0,0 +1,8 @@
+//
+//  APIRequests.swift
+//  Doorbell
+//
+//  Created by Cora Bleier on 20.01.23.
+//
+
+import Foundation
---
+++ frontend/Doorbell/Doorbell/DoorbellRing.swift
@@ -0,0 +1,12 @@
+//
+//  DoorbellRing.swift
+//  Doorbell
+//
+//  Created by Cora Bleier on 20.01.23.
+//
+
+import Foundation
+
+public class DoorbellRing: Codable {
+    var timestamp: String = ""
+}
